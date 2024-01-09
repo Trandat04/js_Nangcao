d48ee742fa5f196cea7be13265bad575600ac8e7
Trandat04 <149884022+Trandat04@users.noreply.github.com>
2024-01-09 12:45:15 +0700

    index

---
+++ 01/scrip.js
@@ -0,0 +1,115 @@
+// console.log(100)
+// console.log('dattc');
+
+// console.log(20, 'Hello', true);
+
+// const x = 100;
+// console.log(x);
+
+// console.error('Alert');
+// console.warn('Warning');
+
+// console.table({ name: 'Brad', email: 'brad@gmail.com' });
+
+// console.group('simple');
+// console.log(x);
+// console.error('Alert');
+// console.warn('Warning');
+// console.groupEnd();
+
+// const styles = 'padding: 10px; background-color: pink ; color: green';
+// console.log('%cHello World', styles);
+
+// bài3: khai báo biến
+$ten_bien='';
+// Ways to declare a variable
+// `var`, `let`, & `const`
+const myName ='Dat';
+console.log(myName);
+
+let age =18;
+age=19;
+console.log(age);
+
+// const firstName = 'Dat09';
+// $my_name ='Dat';//snake case 
+// const lastName = 'Doe';//camelCase
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+// console.log(firstName);
+
+//uu tien const
+
+// let score=1;
+// // score = 1;
+// // console.log(score);
+
+// if (age > 18) {
+//   score = score + 1;
+// }
+
+// console.log(score);
+
+// const x = 100;
+
+
+
+// const arr = [1, 2, 3, 4];
+// arr.push(5);
+// console.log(arr);
+
+// const arr1=['dat', 'hieu', 'canh'];
+// arr1.push('chung');
+// console.log(arr1);
+
+// // object {} -OOP Class
+// let myName1= 'dattc';
+// const person={
+//   name: 'dattc',
+//   age: 18,
+//   hasVo: false,
+//   hasCar:true,
+
+// };
+// person.age = 19;
+// console.log("person", person);
+
+//biaf04 kieu du lieu
+//string
+const firstName = 'Sara';
+ //number
+ const sge=1;
+ const temp= 99.9;
+
+//boolean
+const hasKids = true;
+
+//null
+const aptNumber = null;
+
+//undefined
+const score = undefined;
+
+// // Symbol
+// const id = Symbol('id');
+
+// Reference Types
+//arr 
+const numbers = [1, 2, 3, 4];
+// object
+const person = {
+  name: 'Brad',
+};
+
+//function 
+function getPr() {
+    console.log("Hello");
+}
+getPr();
+
+console.log(hasKids,aptNumber,score)
+
+
+
